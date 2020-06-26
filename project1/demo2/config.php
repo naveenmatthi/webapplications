f0a0efe7e895a1b48d0ea9bc3436192c890b69d7
naveenmatthi <36219106+naveenmatthi@users.noreply.github.com>
2020-06-26 17:20:50 +0530

    Delete config.php

--- project1/demo2/config.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'project1');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
